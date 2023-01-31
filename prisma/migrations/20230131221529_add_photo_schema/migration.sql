49416b03c18a3ce9f0e9476caa6f8c08d97e6ddf
brandon-schabel <brandonschabel1995@gmail.com>
2023-01-31 15:22:01 -0700

    update migrations

---
+++ prisma/migrations/20230131221529_add_photo_schema/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Photo" (
+    "id" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Photo_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Photo" ADD CONSTRAINT "Photo_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
